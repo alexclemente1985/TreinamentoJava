9e83a6714ae3d971b0582cf90a60f1a6375b1c10
alexclemente1985 <alexclemente1985@gmail.com>
2022-04-11 21:22:18 -0300

    Criação do projeto java

---
+++ src/main/java/com/alexandre/api/MyFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.alexandre.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+		System.out.println("eita");
+	}
+
+}
